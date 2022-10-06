3f988892ce11e56386beff94005134a96370e7a6
sumit <sumitagrawal354@gmail.com>
2022-10-05 19:37:20 -0700

    initial commit

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -10,7 +10,21 @@ const Header = () => {
             boxShadow: 'none',
             padding: '10px 0px'
         }
-        }>
+        }
+        >
+            <Toolbar>
+                <div className= "Header_logo">
+                    <div className="font_righteous header_logo_venu">The Venu</div>
+                    <div className="header_logo_title">Musical Events</div>
+
+                </div>
+            </Toolbar>
+            <IconButton
+            aria-label="Menu"
+            color="inherit"
+            
+            ><MenuIcon/>
+            </IconButton>
 
         </AppBar>
     );
