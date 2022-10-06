afe4831bdc3bc9078258fc4568f484fe47ef4487
sumit <sumitagrawal354@gmail.com>
2022-10-06 11:34:02 -0700

    initial commit

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -4,28 +4,28 @@ import { Toolbar, IconButton } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
 const Header = () => {
     return(
-        <AppBar position="Fixed" style={
-         {
-            backgroundColor: '#2f2f2f',
-            boxShadow: 'none',
-            padding: '10px 0px'
-        }
-        }
+        <AppBar 
+            position="Fixed" 
+            style={{
+                backgroundColor: '#2f2f2f',
+                boxShadow: 'none',
+                padding: '10px 0px'
+            }}
         >
             <Toolbar>
-                <div className= "Header_logo">
-                    <div className="font_righteous header_logo_venu">The Venu</div>
+                <div className= "header_logo">
+                    <div className="font_righteous header_logo_venue">The Venue</div>
                     <div className="header_logo_title">Musical Events</div>
 
                 </div>
-            </Toolbar>
-            <IconButton
-            aria-label="Menu"
-            color="inherit"
             
-            ><MenuIcon/>
+            <IconButton
+                aria-label="Menu"
+                color="inherit"
+            >
+                <MenuIcon/>
             </IconButton>
-
+            </Toolbar>
         </AppBar>
     );
 }
